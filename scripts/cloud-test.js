45f15d135654fb608d8b3859eb018e3ad004e198
Michael Wanyoike <micwanyoike@gmail.com>
2021-10-06 13:17:31 +0300

    spacing fix

--- scripts/cloud-test.js
+++ scripts/cloud-test.js
@@ -1,21 +1,20 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
-
+import http from "k6/http";
+import { sleep } from "k6";
 
 export const options = {
-    ext: {
-        loadimpact: {
-            name: 'cloud-test',
-        },
+  ext: {
+    loadimpact: {
+      name: "cloud-test",
     },
-    duration: '1m',
-    vus: 50,
-    thresholds: {
-        http_req_duration: ['p(95)<500'],
-    },
+  },
+  duration: "1m",
+  vus: 50,
+  thresholds: {
+    http_req_duration: ["p(95)<500"],
+  },
 };
 
 export default function () {
-    const res = http.get('https://test.k6.io');
-    sleep(1);
+  const res = http.get("https://test.k6.io");
+  sleep(1);
 }
--- scripts/test.js
+++ scripts/test.js
@@ -1,7 +1,7 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
+import http from "k6/http";
+import { sleep } from "k6";
 
 export default function () {
-    const res = http.get('https://test.k6.io');
-    sleep(1);
+  const res = http.get("https://test.k6.io");
+  sleep(1);
 }
